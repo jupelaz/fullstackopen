7f407d6a0c17302d3c2fd8f7ea780168414ecc7d
linkg <linkguardia@gmail.com>
2021-06-21 07:05:16 +0200

    graphql frontend

---
+++ test/graphql-frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useQuery } from '@apollo/client'
+import { Persons } from './components/Persons'
+import { PersonForm } from './components/PersonForm'
+import { ALL_PERSONS } from './queries'
+
+export const App = () => {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  return (
+    <div>
+      <Persons persons={result.data.allPersons} />
+      <PersonForm />
+    </div>
+  )
+}
