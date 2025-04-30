28826f6e732f028772aa49dd9e101f9d28064cc7
jupelaz <jupelaz@gmail.com>
2025-04-30 08:15:19 +0200

    part1 swap content with part

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,4 +1,4 @@
-import Content from './Content'
+import Part from './Part'
 import Header from './Header'
 import Total from './Total'
 const App = () => {
@@ -13,7 +13,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Part part={part1} exercises={exercises1} /> 
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
       <Total sum={exercises1 + exercises2 + exercises3} />
     </div>
   )
